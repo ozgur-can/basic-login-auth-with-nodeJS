d5a645a7271a545d1d86206d6749dddb1160248f
Özgür Can Altınok <ozgurcanaltinok@gmail.com>
2022-03-30 16:01:10 +0300

    basic login auth with static json db

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const db = require("./db.json");
 
 const app = express();
 const port = process.env.PORT || "8000";
@@ -7,15 +8,31 @@ const port = process.env.PORT || "8000";
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname, "public")));
-
-// app.get("/", (req, res) => {
-//     res.status(200).send("OZGUR");
-// });
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.render("index", { title: "Home" });
+  res.render("index", { title: "Home" });
+});
+
+app.post("/login", (req, res) => {
+  const bodyExist = req.body;
+
+  if (!bodyExist || bodyExist.username === '' || bodyExist.password === '') {
+    res.render("error", {
+      message: "form is empty",
+    });
+  } else if (bodyExist) {
+    const username = bodyExist.username;
+    const password = bodyExist.password;
+
+    if (db.username !== username || db.password !== password) {
+      res.render("error", {
+        message: "username or password is wrong",
+      });
+    }
+  } else res.render("profile", { title: "Profile" });
 });
 
 app.listen(port, () => {
-    console.log(`Listening to requests on http://localhost:${port}`);
+  console.log(`Listening to requests on http://localhost:${port}`);
 });
---
+++ public/zgr.js
@@ -0,0 +1,3 @@
+function deneme() {
+    alert("zgr")
+}
